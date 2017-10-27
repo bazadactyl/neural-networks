8dc4f3eab09df07293bafad8fd9c19ffa622b7a2
Christian Abbott <christianabbott66@gmail.com>
2017-10-27 01:05:39 -0400

    Add files via upload

--- feedforward.py
+++ feedforward.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sklearn as sk
 from load_mnist_data import load_mnist_data
 
 
@@ -12,9 +13,10 @@ def sigmoid(x, inverse=False):
 
 
 class FFNet:
-    def __init__(self, arch=np.array([784, 40, 20, 10])):
-        self._sample = np.random.normal(0, 1, size=(28, 28)).flatten().reshape(784, 1)
+    def __init__(self, arch=np.array([784, 40, 20, 10]), lr=0.1):
         self._arch = arch
+        self._batch_size = 128
+        self._lr = lr
         self._num_layers = len(self._arch)
         self._init_w()
         self._init_b()
@@ -24,29 +26,97 @@ class FFNet:
         self.input_batches = None
         self.target_batches = None
 
+
     def _init_w(self, low=0, high=1):
+        ''' Initialize weights using gaussian distribution '''
         self._w = [np.random.normal(low, high, size=(j, i)) for i, j in zip(self._arch[:-1], self._arch[1:])]
 
+
     def _init_b(self, low=0, high=1):
+        ''' Initialize biases using gaussian distribution '''
         self._b = [np.random.normal(low, high, size=(i, 1)) for i in self._arch[1:]]
 
+
+    def _activate(self, z, inverse=False):
+        ''' Sigmoid activation function '''
+        return sigmoid(z, inverse)
+
+
+    def _calculate_z(self, x, n):
+        ''' Calculate raw output value z at layer n '''
+        return np.dot(self._w[n], x) + self._b[n]
+
+
+    def _propagate(self, x):
+        ''' Calculate the activation and output values at each layer given
+            input x '''
+        self._a = []
+        self._o = []
+        tmp = x
+
+        for l in range(self._num_layers - 1):
+            tmp = self._calculate_z(tmp, l)
+            self._o.append(tmp)
+            tmp = self._activate(tmp)
+            self._a.append(tmp)
+
+        return self._a, self._o
+
+
+    def _backpropagate(self, y):
+        ''' Propagate error signal backward from output layer '''
+        self._d = []
+
+        ''' Calculating the gradient for the output layer l = n '''
+        out_d = np.multiply((self._a[-1] - y), 
+                             self._activate(self._o[-1], inverse=True))
+        self._d.append(out_d)
+
+        ''' Calculating the gradient for all layers l = 0 ... n-1 '''
+        for l in range(self._num_layers-2, 0, -1):
+            d = np.multiply(np.dot((self._w[l]).T, self._d[l-2]), 
+                            sigmoid(self._o[l-1], inverse=True))
+            self._d.append(d)
+
+        ''' Reversal of the list of gradients '''
+        self._d = self._d[::-1]
+        return self._d
+
+
+    def _adjust_weights(self):
+        ''' Adjust weights according to gradients self._d '''
+        self._w = [w - (self._lr / self._batch_size) * np.dot(d, a.T)
+                   for w, d, a in zip(self._w, self._d, self._a)]
+        return self._w
+
+
+    def _adjust_biases(self):
+        ''' Adjust biases according to gradients self._d '''
+        self._b = [b - (self._lr / self._batch_size) * (np.sum(d, axis=1)).reshape(b.shape)
+                   for b, d in zip(self._b, self._d)]
+        return self._b
+
+
     def _prepare_train_inputs(self, images):
-        """Arrange the inputs such that each column respresents the pixels of one image."""
+        ''' Arrange the inputs such that each column respresents the pixels of one image. '''
         self.train_inputs = images.T
         self.training_set_size = self.train_inputs.shape[1]
 
+
     def _prepare_train_targets(self, labels):
-        """Arrange the targets such that each column represents the desired output for an image."""
+        ''' Arrange the targets such that each column represents the desired output for an image. '''
         labels = labels.T[0]
         self.train_targets = np.zeros((10, self.training_set_size))
         for i, label in enumerate(labels):
             self.train_targets[label][i] = 1
 
+
     def _shuffle_training_set(self):
-        """Permute the inputs. Yields better results during training."""
+        ''' Permute the inputs. Yields better results during training. '''
         permutation = np.random.permutation(self.training_set_size)
         self.train_inputs = self.train_inputs.T[permutation].T
         self.train_targets = self.train_targets.T[permutation].T
+
 
     def _batch_training_data(self, batch_size):
         num_images = self.train_inputs.shape[1]
@@ -63,40 +133,17 @@ class FFNet:
             target_batch = np.array([row[start:end] for row in self.train_targets])
             self.target_batches.append(target_batch)
 
-    def _activate(self, z):
-        return sigmoid(z)
 
-    def _calculate_z(self, x, n):
-        return np.dot(self._w[n], x) + self._b[n]
-
-    def _propagate(self, x):
-        self._a = []
-        self._o = []
-        tmp = x
-
-        for l in range(self._num_layers - 1):
-            tmp = self._calculate_z(tmp, l)
-            self._o.append(tmp)
-            tmp = self._activate(tmp)
-            self._a.append(tmp)
-
-        return self._a, self._o
-
-    def _backpropagate(self, x, y):
-        # x = final activation
-        # final = multiply((x-y),_activate(x, inverse=True))
-        pass
-
-    def train_network(self, train_images, train_labels, learn_rate=2.0, epochs=50, batch_size=100):
+    def train_network(self, train_images, train_labels, epochs=50):
         self._prepare_train_inputs(train_images)
         self._prepare_train_targets(train_labels)
 
         for epoch in range(epochs):
             self._shuffle_training_set()
-            self._batch_training_data(batch_size)
+            self._batch_training_data(self._batch_size)
 
             for input_batch, target_batch in zip(self.input_batches, self.target_batches):
-                self._feed_forward(input_batch)
+                self._propagate(input_batch[:,0])
 
             # Compute corrections
             print('Hello')
@@ -107,9 +154,11 @@ class FFNet:
 
 def main():
     train_images, train_labels, test_images, test_labels = load_mnist_data()
+    train_labels
     net = FFNet()
-    net.train_network(train_images, train_labels)
+
 
 
 if __name__ == '__main__':
     main()
+
