c332e430018859df5d2093de579676a9a7a91165
Basim Ramadhan <basim.ramadhan@gmail.com>
2017-11-10 10:10:19 -0500

    Implemented Hopfield network training and recovering

--- a3/hopfieldnet.py
+++ a3/hopfieldnet.py
@@ -20,22 +20,67 @@ class HopfieldNetwork:
         self._w = np.zeros((self._shape, self._shape), dtype=np.float64)
         return self._w
 
-    def _train(self, X):
+    # Calculate global energy
+    def _global_energy(self, active, threshold):
+        num_neurons = len(active)
+        energy = 0.0
+        for neuron_i in range(num_neurons):
+            energy += threshold[neuron_i] * active[neuron_i]
+            for neuron_j in range(num_neurons):
+                energy += -0.5 * self._w[neuron_i][neuron_j] * active[neuron_i] * active[neuron_j]
+        return energy
+
+
+    # Calculate quadratic energy function
+    def _qef(self):
+        pass
+
+    def train(self, X):
         for x in X:
             a = x.reshape((784, 1))
             b = a.T
             self._w += np.dot(a, b)
 
-        self._w -= (np.identity(len(x)) * X.shape[0])
+        self._w -= (np.identity(x.size) * X.shape[0])
         return self._w
 
-    # Calculate global energy
-    def _global_energy(self):
-        pass
+    def recover(self, x, threshold=0.0):
+        num_neurons = x.size
+        active = np.random.randint(2, size=num_neurons)
+        threshold = [threshold] * num_neurons
+        for i, _ in enumerate(active):
+            active[i] = -1 if active[i] == 0 else 1
+        history = []
+        iteration = 0
+        while not self.stable(history):
+            neuron_i = random.choice(range(num_neurons))
+            weight_sum = 0.0
+            for neuron_j in range(num_neurons):
+                weight_sum += self._w[neuron_i][neuron_j] * active[neuron_j]
+            active[neuron_i] = 1 if weight_sum > threshold[neuron_i] else -1
+            iteration += 1
+            if iteration % 200 == 0:
+                energy = self._global_energy(active, threshold)
+                history.append(energy)
 
-    # Calculate quadratic energy function
-    def _qef(self):
-        pass
+    @staticmethod
+    def degrade(x, noise):
+        pixels_to_alter = x.size // noise
+        for _ in range(pixels_to_alter):
+            pixel = random.choice(range(x.size))
+            x[pixel] = 1 if x[pixel] == -1 else -1
+        return x
+
+    @staticmethod
+    def stable(energy_history, check_last=5):
+        if len(energy_history) < check_last:
+            return False
+        recent_states = energy_history[-check_last:]
+        for i in range(check_last - 1):
+            if recent_states[i] != recent_states[i + 1]:
+                return False
+        return True
+
 
 def main():
     # Load and prepare data
@@ -50,14 +95,26 @@ def main():
 
     # Convert X and y lists to numpy arrays
     X, y = (np.asarray(X, dtype=np.float64), np.asarray(y, dtype=np.uint64))
-    X = (X/128)-1
-    X = X[0:100]
-    y = y[0:100]
 
+    # Regularize the training examples
+    for row in range(X.shape[0]):
+        for col in range(X.shape[1]):
+            X[row][col] = 1.0 if X[row][col] > 0.0 else -1.0
+
+    # X = X[0:1]
+    # y = y[0:1]
+
+    X = X[-1:]
+    y = y[-1:]
 
     # Initialize network
     net = HopfieldNetwork()
-    print(net._train(X))
+    print(net.train(X))
+
+    # Test the network
+    x = net.degrade(X[0], 5)
+    x = net.recover(x)
+
 
 
 if __name__ == '__main__':
