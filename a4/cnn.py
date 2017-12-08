f1b737a152a2cd3fb0cb9b284473456629fbf58d
Christian Abbott <christianabbott66@gmail.com>
2017-12-08 17:01:06 -0500

    Added Tensorboard support in python3

--- a4/cnn.py
+++ a4/cnn.py
@@ -3,69 +3,63 @@ import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 from keras.datasets import cifar10
 
-batch_size = 64
+batch_size = 256
 test_size = 256
 epochs = 200
+logs_path = '/tmp/tensorboard_a4/'
 
 
 def init_weights(shape):
     return tf.Variable(tf.random_normal(shape, stddev=0.01))
 
+def _conv2d(X, weights, name, strides=[1,1,1,1], padding='SAME'):
+    with tf.name_scope(name) as scope:
+        return tf.nn.conv2d(X,
+                            weights,
+                            strides=strides,
+                            padding=padding,
+                            name=name)
+
+def _relu(X, name):
+    with tf.name_scope(name) as scope:
+        return tf.nn.relu(X)
+
+def _max_pool(X, name, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME'):
+    with tf.name_scope(name) as scope:
+        return tf.nn.max_pool(X,
+                              ksize=ksize,
+                              strides=strides,
+                              padding=padding,
+                              name=name)
+
+def _dropout(X, sigma, name):
+    with tf.name_scope(name) as scope:
+        return tf.nn.dropout(X, sigma)
+
+def _matmul(X, y, name):
+    with tf.name_scope(name) as scope:
+        return tf.matmul(X, y)
+
 def model(X, w_1, w_2, w_fc, w_o, p_keep_conv, p_keep_hidden):
-    l1a = tf.nn.relu(tf.nn.conv2d(X,
-                                  w_1,
-                                  strides=[1, 1, 1, 1],
-                                  padding='SAME',
-                                  name="conv1"))
+    l1a = _conv2d(X, w_1, name='conv1')
+    l1 = _relu(l1a, name='relu1')
+    l1 = _max_pool(l1, name='pool1')
+    l1 = _dropout(l1, sigma=p_keep_conv, name='dropout1')
 
-    l1 = tf.nn.max_pool(l1a,
-                        ksize=[1, 3, 3, 1],
-                        strides=[1, 2, 2, 1],
-                        padding='SAME',
-                        name="pool1")
-
-    l1 = tf.nn.dropout(l1, p_keep_conv)
-
-    l2 = tf.nn.relu(tf.nn.conv2d(l1,
-                                 w_2,
-                                 strides=[1, 1, 1, 1],
-                                 padding="SAME",
-                                 name="conv2"))
-
-    l2 = tf.nn.max_pool(l2,
-                        ksize=[1, 3, 3, 1],
-                        strides=[1, 2, 2, 1],
-                        padding="SAME",
-                        name="pool2")
-
-    l2 = tf.nn.dropout(l2, p_keep_conv)
+    l2 = _conv2d(l1, w_2, name='conv2')
+    l2 = _relu(l2, name='relu2')
+    l2 = _max_pool(l2, name='pool2')
+    l2 = _dropout(l2, sigma=p_keep_conv, name='dropout2')
 
     l3 = tf.reshape(l2, [-1, w_fc.get_shape().as_list()[0]])
-    l3 = tf.nn.dropout(l3, p_keep_conv)
+    l3 = _dropout(l3, sigma=p_keep_conv, name='dropout3')
 
-    l4 = tf.nn.relu(tf.matmul(l3, w_fc))
-    l4 = tf.nn.dropout(l4, p_keep_hidden)
+    l4 = _matmul(l3, w_fc, name='matmul1')
+    l4 = _relu(l4, name='relu3')
+    l4 = _dropout(l4, sigma=p_keep_hidden, name='dropout4')
 
-    pyx = tf.matmul(l4, w_o)
+    pyx = _matmul(l4, w_o, name='pyx')
     return pyx
-
-
-# def model(X, w, w_fc, w_o, p_keep_conv, p_keep_hidden):
-#     l1a = tf.nn.relu(tf.nn.conv2d(X, w,                       # l1a shape=(?, 28, 28, 32)
-#                         strides=[1, 1, 1, 1], padding='SAME'))
-#     l1 = tf.nn.max_pool(l1a, ksize=[1, 2, 2, 1],              # l1 shape=(?, 14, 14, 32)
-#                         strides=[1, 2, 2, 1], padding='SAME')
-#     l1 = tf.nn.dropout(l1, p_keep_conv)
-#
-#
-#     l3 = tf.reshape(l1, [-1, w_fc.get_shape().as_list()[0]])    # reshape to (?, 14x14x32)
-#     l3 = tf.nn.dropout(l3, p_keep_conv)
-#
-#     l4 = tf.nn.relu(tf.matmul(l3, w_fc))
-#     l4 = tf.nn.dropout(l4, p_keep_hidden)
-#
-#     pyx = tf.matmul(l4, w_o)
-#     return pyx
 
 
 encoder = OneHotEncoder()
@@ -85,11 +79,19 @@ p_keep_conv = tf.placeholder("float")
 p_keep_hidden = tf.placeholder("float")
 py_x = model(X, w_1, w_2, w_fc, w_o, p_keep_conv, p_keep_hidden)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
-train_op = tf.train.RMSPropOptimizer(0.0001, 0.9).minimize(cost)
-predict_op = tf.argmax(py_x, 1)
+with tf.name_scope("cost") as scope:
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y))
+
+with tf.name_scope("train_op") as scope:
+    train_op = tf.train.RMSPropOptimizer(0.0001, 0.9).minimize(cost)
+
+with tf.name_scope("predict_op") as scope:
+    predict_op = tf.argmax(py_x, 1)
+    tf.summary.scalar("predict_op", predict_op)
 
 with tf.Session() as sess:
+    summary_op = tf.summary.merge_all()
+    writer = tf.summary.FileWriter(logs_path, sess.graph)
     tf.global_variables_initializer().run()
 
     for i in range(epochs):
@@ -98,11 +100,11 @@ with tf.Session() as sess:
         training_batches = zip(batch_starts, batch_ends)
 
         for start, end in training_batches:
-            sess.run(train_op, feed_dict={
+            train_graph = sess.run(train_op, feed_dict={ 
                 X: X_train[start:end],
                 Y: y_train[start:end],
                 p_keep_conv: 0.6,
-                p_keep_hidden: 0.9,
+                p_keep_hidden: 0.9, 
             })
 
         test_indices = np.arange(len(X_test))
